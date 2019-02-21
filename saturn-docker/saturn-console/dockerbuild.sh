ddf0646da34fe680b436cdbc95c985a82482596c
pjsong <pjsong3101@163.com>
2019-02-21 22:02:51 +0800

    fix

--- saturn-docker/saturn-console/dockerbuild.sh
+++ saturn-docker/saturn-console/dockerbuild.sh
@@ -1,4 +1,4 @@
-CONSOLE_DL_URL="https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-console&c=exec&v=${VERSION}"
 VERSION=3.1.0
 REPO=releases
+CONSOLE_DL_URL="https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-console&c=exec&v=${VERSION}"
 docker build --build-arg SATURN_CONSOLE_DOWNLOAD_URL=$CONSOLE_DL_URL -t saturn/saturn-console ./saturn-console/
