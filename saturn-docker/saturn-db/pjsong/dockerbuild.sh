24a0ae25cf746b8bd2e8ffca7fdc8c8fc3e0bea7
pjsong <pjsong3101@163.com>
2019-02-22 00:02:05 +0800

    saturn db docker

--- saturn-docker/saturn-db/pjsong/dockerbuild.sh
+++ saturn-docker/saturn-db/pjsong/dockerbuild.sh
@@ -1,3 +1,4 @@
-docker build --build-arg usr="root" --build-arg pwd="defaultpass" -t pjsong/saturn-db .
+#docker build --build-arg usr="root" --build-arg pwd="defaultpass" -t pjsong/saturn-db .
+docker build -t pjsong/saturn-db .
 
 # related file, docker-compose.yml has defaultpass as environment variable
