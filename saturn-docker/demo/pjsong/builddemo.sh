a97a4f9a4ae8ac1d88135270ddb45054abe7f84c
pjsong <pjsong3101@163.com>
2019-02-24 15:33:38 +0800

    executor

---
+++ saturn-docker/demo/pjsong/builddemo.sh
@@ -0,0 +1,2 @@
+EXECUTOR_DL_URL="https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-executor&v=${VERSION}&e=zip&c=zip"
+docker build --build-arg SATURN_EXECUTOR_DOWNLOAD_URL=$EXECUTOR_DL_URL -t saturn/demo-init .
