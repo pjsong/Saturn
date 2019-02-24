dec0cdc1e4ed1a03b0cc73c32c89bf35b5b0e0d2
pjsong <pjsong3101@163.com>
2019-02-24 16:47:26 +0800

    fix

--- saturn-docker/demo/pjsong/builddemo.sh
+++ saturn-docker/demo/pjsong/builddemo.sh
@@ -1,2 +1,4 @@
+REPO=releases
+VERSION=3.1.0
 EXECUTOR_DL_URL="https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-executor&v=${VERSION}&e=zip&c=zip"
 docker build --build-arg SATURN_EXECUTOR_DOWNLOAD_URL=$EXECUTOR_DL_URL -t saturn/demo-init .
